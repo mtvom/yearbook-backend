90ab29e9558f3736a00c28c5c7eafb0d22c6c3d8
Tereza Veríssimo <mtvom@aluno.ifnmg.edu.br>
2026-06-18 22:51:05 +0000

    feat: desafio 02

--- temp-desafio.js
+++ temp-desafio.js
@@ -1,7 +1,27 @@
 import prisma from './prisma/client.js';
-
+console.log ("DESAFIO 01");
 const alunoInexistente =  await prisma.aluno.findUnique({ where: { id: 999 } });
 console.log (alunoInexistente);
+
+console.log ("\nDESAFIO 02");
+const listaSEMsenha = await prisma.aluno.findMany({
+    select: {
+        id: true,
+        nome: true, 
+        email: true,
+        cidade: true, 
+        frase: true, 
+        planosFuturos: true, 
+        fotoUrl: true, 
+        role: true, 
+        criadoEm: true
+    },
+});
+
+console.log (listaSEMsenha);
+
+console.log ("\nDESAFIO 03");
+
 
 
 await prisma.$disconnect();
